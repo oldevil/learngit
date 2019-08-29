0d2e39f6b35ff2b6a803c6b1b87defd9d6cdcf08
oldm <oldm@test.com>
2019-08-29 15:27:25 +0800

    update countURL.py: use str.count()

--- countURL.py
+++ countURL.py
@@ -1,10 +1,6 @@
 def countURL(file='bi_aiops.log', target='/api/bi/user/user_info/'):
-    count = 0
     with open(file, 'r') as f:
-        for i in f.readlines():
-            if target in i:
-                count += 1
-    return count
+        return f.read().count(target)
     
 if __name__ == '__main__':
     print(countURL())
